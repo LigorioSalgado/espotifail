f380b1d5fab71cade9ae605c055709ca20479c5c
Ligorio Edwin Salgado Flores <lesf@MacBook-Pro-de-Ligorio.local>
2017-11-21 21:46:26 -0600

    cambios produccion 4

--- espotifail/wsgi.py
+++ espotifail/wsgi.py
@@ -7,7 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
+
 import os
+from dotenv import load_dotenv
+
+
 try:
     dotenv_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '.env')
     load_dotenv(dotenv_path)
@@ -20,5 +24,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "espotifail.settings.production"
 
 application = get_wsgi_application()
 
-from dotenv import load_dotenv
 

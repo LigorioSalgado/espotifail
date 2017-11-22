013ef7caf01da7d1fff56c8a1c81ee79a5329bbb
Ligorio Edwin Salgado Flores <lesf@MacBook-Pro-de-Ligorio.local>
2017-11-21 21:40:35 -0600

    cambios produccion 4

--- espotifail/wsgi.py
+++ espotifail/wsgi.py
@@ -8,6 +8,11 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+try:
+    dotenv_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '.env')
+    load_dotenv(dotenv_path)
+except:
+    pass
 
 from django.core.wsgi import get_wsgi_application
 
@@ -16,8 +21,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "espotifail.settings")
 application = get_wsgi_application()
 
 from dotenv import load_dotenv
-try:
-    dotenv_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '.env')
-    load_dotenv(dotenv_path)
-except:
-    pass
+
